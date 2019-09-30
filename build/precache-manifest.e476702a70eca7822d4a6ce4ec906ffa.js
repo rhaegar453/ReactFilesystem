afa463ab452e06973886e2eab0ef829cb48f4584
Shivaraj Bakale <shivarajapple@gmail.com>
2019-09-30 16:23:02 +0530

    Application Complete

---
+++ build/precache-manifest.e476702a70eca7822d4a6ce4ec906ffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7ba6971f07128a9e2ff1a822be158d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9dc2f9f04f5ba58a8d5",
+    "url": "/static/css/main.13c5a9fd.chunk.css"
+  },
+  {
+    "revision": "a385a88c25b71f1f4e16",
+    "url": "/static/js/2.c917277b.chunk.js"
+  },
+  {
+    "revision": "c9dc2f9f04f5ba58a8d5",
+    "url": "/static/js/main.fbc5de80.chunk.js"
+  },
+  {
+    "revision": "b57bf20a576ff976f3e9",
+    "url": "/static/js/runtime-main.780f656e.js"
+  }
+]);
